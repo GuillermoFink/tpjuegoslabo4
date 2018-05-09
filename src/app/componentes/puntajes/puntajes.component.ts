7e66ba30f5361ec70df8ae198f7fd29ff6fc98f5
Gui <guillermo.fink@bsg.global>
2018-05-09 20:36:27 -0300

    update todos los juegos

--- src/app/componentes/puntajes/puntajes.component.ts
+++ src/app/componentes/puntajes/puntajes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { JugadorService } from '../../servicios/jugador.service';
+import { HttpModule, Http } from '@angular/http';
+import {TableModule} from 'primeng/table';
 
 @Component({
   selector: 'app-puntajes',
@@ -6,10 +9,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./puntajes.component.css']
 })
 export class PuntajesComponent implements OnInit {
+  nombreJugador: string;
+  apodoJugador: string;
+  idJugador: number;
+  apellidoJugador: string;
+  puntajeIndividual: any;
+  tablaPuntaje: any;
 
-  constructor() { }
+  constructor(private miJugador: JugadorService, private miHttp: Http) { 
+    this.nombreJugador = miJugador.GetNombre();
+    this.apellidoJugador = miJugador.GetApellido();
+    this.apodoJugador = miJugador.GetApodo();
+    this.idJugador = miJugador.GetId();
+    this.puntajeIndividual = this.MostrarPuntajePorJugador();
+    this.tablaPuntaje = this.TraerPuntajes();
+  }
 
   ngOnInit() {
   }
 
+  MostrarPuntajePorJugador(){
+    let rta: any;
+    let datos = { usuario: this.idJugador};
+    this.miHttp.post('http://localhost/APITPJUEGOS/api-master/PuntajePorJugador', datos)
+      .toPromise()
+      .then(data => {
+        rta = data.json();
+        console.log(rta);
+        this.puntajeIndividual = rta;
+        
+      })
+  }
+  TraerPuntajes(){
+    this.miHttp.get('http://localhost/APITPJUEGOS/api-master/TodosLosPuntajes')
+      .toPromise()
+      .then(data => {
+        console.log(data.json());
+        this.tablaPuntaje = data.json();
+      })
+  }
 }
